ea676c2b5e7024a6efb44e05fcdd125f2748fdb8
datasciencie25@gmail.com <130195315+vamm99@users.noreply.github.com>
2025-11-17 00:17:55 -0500

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -9,9 +9,12 @@ async function bootstrap() {
   const port = configService.get<number>('PORT');
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
   app.enableCors({
-    origin: '*',
-    credentials: true,
-  });
+  origin: 'https://inventory-system-admin-nine.vercel.app', 
+  credentials: true,
+  methods: 'GET,POST,PUT,DELETE,OPTIONS',
+  allowedHeaders: 'Content-Type, Authorization',
+});
+
   await app.listen(port!, () => console.log(`Server running on port ${port}`));
 }
 bootstrap();
